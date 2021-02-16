f8deac18d3ec4afc2db09ecd15d230c3c1cac446
kganin <kolyaganin@outlook.com>
2021-02-16 14:23:09 +0300

    Правки от наставника

--- js/ad.js
+++ js/ad.js
@@ -1,4 +1,4 @@
-import {getAdData, TYPES} from './data.js';
+import {TYPES} from './data.js';
 
 const GUESTS_WORD_FORMS = [
   'гостя',
@@ -15,13 +15,28 @@ const adList = document.querySelector('.map__canvas');
 const adTemplate = document.querySelector('#card').content.querySelector('.popup');
 const ad = adTemplate.cloneNode(true);
 
+const photosContainer = ad.querySelector('.popup__photos');
+const featuresContainer = ad.querySelector('.popup__features');
+
 const getWordForm = (num, wordForms) => {
   if (num > 1 && num < 5) return wordForms[1];
   if (num == 1) return wordForms[0];
   return wordForms[2];
 };
 
-const renderAd = (adData) => {
+const getPhotos = (container, photos) => {
+  container.innerHTML = '';
+  container.insertAdjacentHTML('afterbegin',photos.map((photo) => `<img src="${photo}" class="popup__photo" width="45" height="40" alt="Фотография жилья">`)
+    .join('\n'));
+};
+
+const getFeatures = (container, features) => {
+  container.innerHTML = '';
+  container.insertAdjacentHTML('afterbegin', features.map((feature) => `<li class="popup__feature popup__feature--${feature}"></li>`)
+    .join('\n'));
+};
+
+export const renderAd = (adData) => {
   ad.querySelector('.popup__avatar').src = adData.author.avatar;
   ad.querySelector('.popup__title').textContent = adData.offer.title;
   ad.querySelector('.popup__text--address').textContent = adData.offer.address;
@@ -30,11 +45,7 @@ const renderAd = (adData) => {
   ad.querySelector('.popup__text--capacity').textContent = `${adData.offer.rooms} ${getWordForm(adData.offer.rooms, ROOMS_WORD_FORMS)} для ${adData.offer.guests} ${getWordForm(adData.offer.guests, GUESTS_WORD_FORMS)}`;
   ad.querySelector('.popup__text--time').textContent = `Заезд после ${adData.offer.checkin}, выезд до ${adData.offer.checkout}`;
   ad.querySelector('.popup__description').textContent = adData.offer.description;
-  ad.querySelector('.popup__features').innerHTML = '';
-  ad.querySelector('.popup__features').insertAdjacentHTML('afterbegin', adData.offer.features.map((feature) => `<li class="popup__feature popup__feature--${feature}"></li>`).join('\n'));
-  ad.querySelector('.popup__photos').innerHTML = '';
-  ad.querySelector('.popup__photos').insertAdjacentHTML('afterbegin', adData.offer.photos.map((photo) => `<img src="${photo}" class="popup__photo" width="45" height="40" alt="Фотография жилья"></img>`).join('\n'));
+  getPhotos(photosContainer, adData.offer.photos);
+  getFeatures(featuresContainer, adData.offer.features);
   adList.appendChild(ad);
 };
-
-renderAd(getAdData());
